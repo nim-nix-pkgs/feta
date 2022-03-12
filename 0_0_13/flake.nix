e4a88a7984bc01c6febd4f6c594822b0b805f554
hugosenari <hugosenari@gmail.com>
2022-03-12 02:16:36 -0300

    chore: re index feta

---
+++ 0_0_13/flake.nix
@@ -0,0 +1,58 @@
+{
+  description = ''A domain-specific for general purpose office automation. The language is embedded in Nim and allows for quick and easy integration of different office software environments.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-feta-0_0_13.flake = false;
+  inputs.src-feta-0_0_13.owner = "FlorianRauls";
+  inputs.src-feta-0_0_13.ref   = "refs/tags/0.0.13";
+  inputs.src-feta-0_0_13.repo  = "office-DSL-thesis";
+  inputs.src-feta-0_0_13.type  = "github";
+  
+  inputs."mime".dir   = "nimpkgs/m/mime";
+  inputs."mime".owner = "riinr";
+  inputs."mime".ref   = "flake-pinning";
+  inputs."mime".repo  = "flake-nimble";
+  inputs."mime".type  = "github";
+  inputs."mime".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mime".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."jester".dir   = "nimpkgs/j/jester";
+  inputs."jester".owner = "riinr";
+  inputs."jester".ref   = "flake-pinning";
+  inputs."jester".repo  = "flake-nimble";
+  inputs."jester".type  = "github";
+  inputs."jester".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jester".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."googleapi".dir   = "nimpkgs/g/googleapi";
+  inputs."googleapi".owner = "riinr";
+  inputs."googleapi".ref   = "flake-pinning";
+  inputs."googleapi".repo  = "flake-nimble";
+  inputs."googleapi".type  = "github";
+  inputs."googleapi".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."googleapi".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."ws".dir   = "nimpkgs/w/ws";
+  inputs."ws".owner = "riinr";
+  inputs."ws".ref   = "flake-pinning";
+  inputs."ws".repo  = "flake-nimble";
+  inputs."ws".type  = "github";
+  inputs."ws".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."ws".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-feta-0_0_13"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-feta-0_0_13";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,91 @@
+{
+  description = ''A domain-specific for general purpose office automation. The language is embedded in Nim and allows for quick and easy integration of different office software environments.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."feta-main".url = "path:./main";
+  inputs."feta-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_0".url = "path:./0_0_0";
+  inputs."feta-0_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_11".url = "path:./0_0_11";
+  inputs."feta-0_0_11".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_11".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_12".url = "path:./0_0_12";
+  inputs."feta-0_0_12".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_12".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_13".url = "path:./0_0_13";
+  inputs."feta-0_0_13".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_13".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_14".url = "path:./0_0_14";
+  inputs."feta-0_0_14".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_14".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_15".url = "path:./0_0_15";
+  inputs."feta-0_0_15".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_15".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_16".url = "path:./0_0_16";
+  inputs."feta-0_0_16".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_16".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_17".url = "path:./0_0_17";
+  inputs."feta-0_0_17".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_17".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_2".url = "path:./0_0_2";
+  inputs."feta-0_0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_3".url = "path:./0_0_3";
+  inputs."feta-0_0_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_4".url = "path:./0_0_4";
+  inputs."feta-0_0_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_5".url = "path:./0_0_5";
+  inputs."feta-0_0_5".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_5".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_6".url = "path:./0_0_6";
+  inputs."feta-0_0_6".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_6".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_7".url = "path:./0_0_7";
+  inputs."feta-0_0_7".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_7".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_8".url = "path:./0_0_8";
+  inputs."feta-0_0_8".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_8".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_0_9".url = "path:./0_0_9";
+  inputs."feta-0_0_9".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_0_9".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."feta-0_1_0".url = "path:./0_1_0";
+  inputs."feta-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."feta-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
